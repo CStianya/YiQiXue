a1dba4dd809619a317dab6705f02ec386e5f8943
CStianya <675121663@qq.com>
2017-05-17 17:38:39 +0800

    添加一些项目文件和资源

---
+++ app/src/main/java/com/cs/yiqixue_mvp/utils/MyApplication.java
@@ -0,0 +1,23 @@
+package com.cs.yiqixue_mvp.utils;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by CSLaker on 2017/5/2.
+ */
+
+public class MyApplication extends Application {
+
+    private static Context sContext;
+
+    @Override
+    public void onCreate() {
+        sContext = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return  sContext;
+    }
+
+}
